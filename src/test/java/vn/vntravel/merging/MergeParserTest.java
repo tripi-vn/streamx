a3f9d70143d19592a3a05e0fc7c95a7f12b454df
Nguyễn Tuấn Anh <anhnt.it@tripi.vn>
2020-03-27 13:55:28 +0700

    create test class

---
+++ src/test/java/vn/vntravel/merging/MergeParserTest.java
@@ -0,0 +1,71 @@
+package vn.vntravel.merging;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Map;
+
+public class MergeParserTest {
+    private List<MergeTopicModel> mergeTopicModels;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    private List<MergeTopicModel> runParseTest(String filename){
+        return new MergeParser().parse(filename);
+    }
+
+    @Test
+    public void TestParserModel() throws Exception{
+        mergeTopicModels = runParseTest("src/test/resources/merge-test.properties");
+        assertEquals("Check number object: ",2, mergeTopicModels.size());
+        assertEquals("Check number topics object1: ", 3, mergeTopicModels.get(0).getTopics().size());
+        assertEquals("Check number topics object2: ",2, mergeTopicModels.get(1).getTopics().size());
+    }
+
+    @Test
+    public void TestFindColumnJoin() throws Exception{
+        mergeTopicModels = runParseTest("src/test/resources/merge-test.properties");
+
+        MergeTopicModel model1 = mergeTopicModels.get(0);
+        assertEquals("Check size column-join object1: ",2, model1.getMergerMaps().size());
+        List<Map<String, String>> mergeMaps1 = model1.getMergerMaps();
+        assertEquals("Check column-map size topic1 in object1: ",2, mergeMaps1.get(0).size());
+        assertEquals("Check column-map size topic2 in object1: ",2, mergeMaps1.get(1).size());
+
+        MergeTopicModel model2 = mergeTopicModels.get(1);
+        assertEquals("Check size column-join object2: ",1, model2.getMergerMaps().size());
+        List<Map<String, String>> mergeMaps2 = model2.getMergerMaps();
+        assertEquals("Check column-map size topic in object2",2, mergeMaps2.get(0).size());
+    }
+
+    @Test
+    public void testFindExcludeAndInclude() throws Exception{
+        mergeTopicModels = runParseTest("src/test/resources/merge-test.properties");
+
+        MergeTopicModel model1 = mergeTopicModels.get(0);
+        assertEquals("Check excludeColumn size object1: ",2, model1.getExcludeColumn().size());
+        assertEquals("Check includeColumn size object1: ",1, model1.getIncludeColumn().size());
+        Map<String, List<String>> excludeModel1 = model1.getExcludeColumn();
+        Map<String, List<String>> includeModel1 = model1.getIncludeColumn();
+        assertEquals("Check values size excludeColumn in object1: ",2, excludeModel1.get("topic2").size());
+        assertEquals("Check values size includeColumn in object1: ",1, includeModel1.get("topic2").size());
+
+        MergeTopicModel model2 = mergeTopicModels.get(1);
+        assertEquals("Check excludeColumn size object2: ",0, model2.getIncludeColumn().size());
+        assertEquals("Check includeColumn size object2: ",0, model2.getExcludeColumn().size());
+    }
+
+    @Test
+    public void testWithErrorFormFile() throws Exception{
+        mergeTopicModels = runParseTest("src/test/resources/wrong-format-test.properties");
+        assertEquals("Check right-format object size: ", 1, mergeTopicModels.size());
+    }
+}
